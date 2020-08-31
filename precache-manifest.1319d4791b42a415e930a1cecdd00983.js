a42642e9bdf0414bf7ef6673afc9dde6e4c6032b
ameedfaridi <faridiameed5@gmail.com>
2020-08-31 20:54:54 +0530

    Updates

---
+++ precache-manifest.1319d4791b42a415e930a1cecdd00983.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9099e57d34826cb3b12e2c5d2319136",
+    "url": "/login/index.html"
+  },
+  {
+    "revision": "af12eb3031023594557f",
+    "url": "/login/static/css/main.9b730775.chunk.css"
+  },
+  {
+    "revision": "dcce596116a302735fea",
+    "url": "/login/static/js/2.e61d2093.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/login/static/js/2.e61d2093.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af12eb3031023594557f",
+    "url": "/login/static/js/main.ea4c1a18.chunk.js"
+  },
+  {
+    "revision": "0d87f729ffe9b79a65fe",
+    "url": "/login/static/js/runtime-main.896c4431.js"
+  }
+]);
